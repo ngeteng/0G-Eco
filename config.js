b0dd90ed7c2e50c03def71fccd0ebfebff0605e0
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-07-26 19:37:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ const FAUCET_CONFIG = {
     tokens: [
         {
             name: "BTC",
-            contractAddress: "ALAMAT_KONTRAK_FAUCET_BTC"
+            contractAddress: "0x36f6414FF1df609214dDAbA71c84f18bcf00F67d"
         },
         {
             name: "ETH",
-            contractAddress: "ALAMAT_KONTRAK_FAUCET_ETH"
+            contractAddress: "0x0fE9B43625fA7EdD663aDcEC0728DD635e4AbF7c"
         },
         {
             name: "USDT",
-            contractAddress: "ALAMAT_KONTRAK_FAUCET_USDT"
+            contractAddress: "0x3eC8A8705bE1D5ca90066b37ba62c4183B024ebf"
         }
     ],
     contractAbi: [
